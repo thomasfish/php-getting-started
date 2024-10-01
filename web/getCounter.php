7c18e02e36998871bed223d3a73f3aa59f7667d3
ThomasFish <iamthomasfish@gmail.com>
2024-10-01 15:38:36 +0100

    increase vote number

--- web/getCounter.php
+++ web/getCounter.php
@@ -2,11 +2,11 @@
 header("Access-Control-Allow-Origin: *");
 header('Cache-Control: no-cache');
 
-/*while (!@mkdir("textfiles/counter.txt.lock")) {
+while (!@mkdir("textfiles/counter.txt.lock")) {
   sleep(1);
-}*/
+}
 $counterfile = file("textfiles/counter.txt");
-//rmdir("textfiles/counter.txt.lock");
+rmdir("textfiles/counter.txt.lock");
 
 $countervalue = $counterfile[0];
 echo $countervalue;
